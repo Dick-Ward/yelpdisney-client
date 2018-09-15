4820013ce3cae1a6c7d9c6de863fbe2e88af532e
Gonzodamus <Dick@TheDickWard.com>
2018-09-15 15:27:57 -0400

    started navbar.  Search needs to be implemented.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return(
-    <div className="navbar">Yelp Disney</div>
+    <React.Fragment>
+      <div className="navbar">Yelp Disney</div>
+      <Link className="homeLink" to="/">Home</Link>
+      <form  className="navSearch" onSubmit={ (e) => {e.preventDefault()}}>
+        <label>Search
+          <input type="text"/>
+          <input type="submit" />
+        </label>
+      </form>
+      </React.Fragment>
   )
 }
 
