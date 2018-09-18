880e12f0da43d69acd448b68b8e11b89de9caf2a
Gonzodamus <Dick@TheDickWard.com>
2018-09-18 07:12:48 -0400

    semantic dropdown and navbar setup

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,10 +7,22 @@ const Navbar = () => {
       <div className="navbar">Yelp Disney</div>
       <Link className="homeLink" to="/">Home</Link>
       <form  className="navSearch" onSubmit={ (e) => {e.preventDefault()}}>
-        <label>Search
+        <label>New Search: 
           <input type="text"/>
-          <input type="submit" />
         </label>
+        {"   in   "}
+        <select defaultValue="" onChange={() => {console.log("hi")}}>
+          <option value="">All Parks</option>
+          <option value="Epcot">Epcot</option>
+          <option value="Magic Kingdom">Magic Kingdom</option>
+          <option value="Hollywood Studios">Hollywood Studios</option>
+          <option value="Animal Kingdom">Animal Kingdom</option>
+          <option value="Blizzard Beach">Blizzard Beach</option>
+          <option value="Typhoon Lagoon">Typhoon Lagoon</option>
+          <option value="Resort Dining">Resort Dining</option>
+        </select>
+        <input type="submit" />
+
       </form>
     </React.Fragment>
   )
