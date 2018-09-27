8f06f6311ef3066c81cda74e6edcad5d543f7fc8
Gonzodamus <Dick@TheDickWard.com>
2018-09-26 21:20:34 -0400

    started styling

--- src/containers/restaurant_list.js
+++ src/containers/restaurant_list.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Restaurant from "../components/restaurant";
-import Options from '../services/data'
 import * as actions from "../actions";
-import {Dropdown} from 'semantic-ui-react'
+import {Grid} from 'semantic-ui-react'
+import Filters from "./filters"
 
 class RestaurantList extends Component {
 
-  handleChange = (event, value) => {
-    if(value.name === "parkFilter"){
-    this.props.applyParkFilter(value.value)
-  } else if (value.name === "cuisineFilter") {
-    this.props.applyCuisineFilter(value.value)
-  }
-  };
-
   render() {
-    console.log(this.props.restaurantList)
+
     const restaurants = this.props.restaurantList.map(restaurant => {
       if (this.props.cuisineFilter !== ""){
         if (restaurant.park.includes(this.props.parkFilter) && restaurant.cuisine && restaurant.cuisine.includes(this.props.cuisineFilter)) {
@@ -36,29 +28,23 @@ class RestaurantList extends Component {
 
 
     return (
-      <div style={{ margin: "10px" }}>
-        <br/>
-        <Dropdown
-          placeholder={'Filter Results by Park'}
-          options={Options.parkOptions}
-          selection
-          onChange={this.handleChange}
-          value={this.props.parkFilter}
-          name="parkFilter"
-          className="filter"
-        />
-        <Dropdown
-          placeholder={'Filter Results by Cuisine'}
-          options={Options.cuisineOptions}
-          selection
-          onChange={this.handleChange}
-          value={this.props.cuisineFilter}
-          name="cuisineFilter"
-          className="filter"
-        />
+      <Grid >
 
-        {this.props.restaurantList.length === 0 ? <div> Loading... </div> : <div>{restaurants}</div>}
-      </div>
+        <Grid.Column width={4}>
+          <Filters />
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <div className="restaurantListContainer">
+            <br/>
+
+            {this.props.restaurantList.length === 0 ? <div> Loading... </div> : <div>{restaurants}</div>}
+          </div>
+        </Grid.Column>
+        <Grid.Column width={4}>
+
+        </Grid.Column>
+      </Grid>
+
     );
   }
 }
