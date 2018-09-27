af937bdf29ba13b6f26118c70cc4fe424f89f880
Gonzodamus <Dick@TheDickWard.com>
2018-09-26 22:04:10 -0400

    category filter in progress

--- src/components/restaurant_card.js
+++ src/components/restaurant_card.js
@@ -1,35 +1,10 @@
 import React from "react";
 import {Link} from 'react-router-dom'
-import {Grid} from 'semantic-ui-react'
+import Options from "../services/data"
 
 const RestaurantCard = ({ restaurant, selectRestaurant, selectedRestaurant }) => {
 
-  const parseCategoryCode = (code) =>{
-    let rest_code
-    switch (code) {
-      case "counter_service":
-        rest_code = "Counter Service"
-        break
-      case "table_service":
-        rest_code = "Table Service"
-        break
-      case "bar_or_lounge":
-        rest_code = "Bar / Lounge"
-        break
-      case "room_service":
-        rest_code = "Room Service"
-        break
-      case "market":
-        rest_code = "Market"
-        break
-      case "food_cart":
-        rest_code = "Food Cart"
-        break
-      default:
-        rest_code = "none"
-    }
-    return rest_code
-  }
+
 
   return (
     <div className="restaurant-card">
@@ -40,7 +15,7 @@ const RestaurantCard = ({ restaurant, selectRestaurant, selectedRestaurant }) =>
 
 
         <div className="right">Cusine: {restaurant.cuisine} </div>
-        <div className="right">Category: {parseCategoryCode(restaurant.category_code)}</div>
+        <div className="right">Category: {Options.parseCategoryCode(restaurant.category_code)}</div>
 
 
         {restaurant.resort_name ? (<p>Resort: {restaurant.resort_name}</p>) : (<p>Park: {restaurant.park}</p>)}
