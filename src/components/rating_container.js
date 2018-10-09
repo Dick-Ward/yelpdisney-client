177a25587a3e2cc3062c7e9ad7b0753d5c58615e
gonzodamus <github email address>
2018-10-09 18:40:32 -0400

    styling more consistant throughout

--- src/components/rating_container.js
+++ src/components/rating_container.js
@@ -4,45 +4,48 @@ import SmallRating from './small_rating'
 
 const RatingContainer = props =>{
   return (
-      <Grid >
-          <Grid.Column width={6} className="rating-text">
 
-          </Grid.Column>
-          <Grid.Column width={4} className="rating-text">
-            <svg style={{width: 0, height: 0}}>
-              <clipPath id="svgPathOverall">
-                 <ellipse cx="47" cy="14" rx="13" ry="13"/>
-                 <ellipse cx="47" cy="46" rx="13" ry="13"/>
-                 <ellipse cx="20" cy="30" rx="20" ry="20"/>
-              </clipPath>
-            </svg>
-            <div>
-              <div className="vertical Overall">
-              <span style={{width: `${(props.rating/5*100) + ((5 - props.rating) * 3)}%`, backgroundColor:"blue"}} />
-              </div>
-              <div> Overall </div>
-              <div>{props.rating}</div>
-            </div>
-          </Grid.Column>
-          <Grid.Column width={6} className="rating-text">
-          </Grid.Column>
+          <Grid >
+              <Grid.Column width={6} className="rating-text">
+              </Grid.Column>
+              <Grid.Column width={4} className="rating-text">
+                <svg style={{width: 0, height: 0}}>
+                  <clipPath id="svgPathOverall">
+                     <ellipse cx="93" cy="26" rx="25" ry="25"/>
+                     <ellipse cx="93" cy="94" rx="25" ry="25"/>
+                     <ellipse cx="40" cy="60" rx="40" ry="40"/>
+                  </clipPath>
+                </svg>
+                <div>
+                  <div className="vertical Overall">
+                  <span style={{width: `${(props.rating/5*100)}%`, backgroundColor:"blue"}} />
+                  </div>
+                  <div> Overall </div>
+                  <div>{props.rating}</div>
+                </div>
+              </Grid.Column>
+              <Grid.Column width={6} className="rating-text">
+              </Grid.Column>
 
-          <Grid.Row >
-            <Grid.Column width={4} className="rating-text">
-              <SmallRating rating={props.quality} ratingOf={"Quality"} color={"red"} />
-            </Grid.Column>
-            <Grid.Column width={4} className="rating-text">
-              <SmallRating rating={props.cleanliness} ratingOf={"Cleanliness"} color={"purple"}/>
-            </Grid.Column>
-            <Grid.Column width={4} className="rating-text">
-              <SmallRating rating={props.service} ratingOf={"Service"} color={"green"}/>
-            </Grid.Column>
-            <Grid.Column width={4} className="rating-text">
-              <SmallRating rating={props.value} ratingOf={"Value"} color={"orange"}/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      )
+              <Grid.Row >
+              <Grid.Column width={2} />
+                <Grid.Column width={3} className="rating-text">
+                  <SmallRating rating={props.quality} ratingOf={"Quality"} color={"red"} />
+                </Grid.Column>
+                <Grid.Column width={3} className="rating-text">
+                  <SmallRating rating={props.cleanliness} ratingOf={"Cleanliness"} color={"purple"}/>
+                </Grid.Column>
+                <Grid.Column width={3} className="rating-text">
+                  <SmallRating rating={props.service} ratingOf={"Service"} color={"green"}/>
+                </Grid.Column>
+                <Grid.Column width={3} className="rating-text">
+                  <SmallRating rating={props.value} ratingOf={"Value"} color={"orange"}/>
+                </Grid.Column>
+                <Grid.Column width={2} />
+              </Grid.Row>
+            </Grid>
+
+        )
       }
 
 export default RatingContainer
