440badf8e574df0d758e8f437ba9b87654b1c508
gonzodamus <github email address>
2018-10-09 16:48:46 -0400

    ratings are now dynamic

--- src/components/small_rating.js
+++ src/components/small_rating.js
@@ -6,16 +6,17 @@ console.log(props)
     <React.Fragment>
     <svg style={{width: 0, height: 0}}>
       <clipPath id={`svgPath${props.ratingOf}`}>
-         <ellipse cx="62" cy="14" rx="13" ry="13"/>
-         <ellipse cx="62" cy="46" rx="13" ry="13"/>
-         <ellipse cx="35" cy="30" rx="20" ry="20"/>
-         <text x="20" y="0" style={{transform: "rotate(90deg)"}}>{props.rating}</text>
+         <ellipse cx="47" cy="14" rx="13" ry="13"/>
+         <ellipse cx="47" cy="46" rx="13" ry="13"/>
+         <ellipse cx="20" cy="30" rx="20" ry="20"/>
       </clipPath>
     </svg>
     <div>
       <div className={`vertical ${props.ratingOf}`}>
-      <span style={{width: `${(props.rating/5*100) + ((5 - props.rating) * 3)}%`, backgroundColor:"red"}} />
+      <span style={{width: `${(props.rating/5*100)}%`, backgroundColor: props.color}} />
       </div>
+      <div>{props.ratingOf}</div>
+      <div>{props.rating}</div>
     </div>
     </React.Fragment>
   )
