c6d1d53699b7aafbc1ffb257589c3318c90ab0c1
Gonzodamus <Dick@TheDickWard.com>
2018-10-13 09:44:27 -0400

    merge error fixed

--- src/components/splash.js
+++ src/components/splash.js
@@ -4,10 +4,8 @@ import * as actions from '../actions/index'
 import Options from '../services/data'
 import {Form, Input, Dropdown, Button} from 'semantic-ui-react'
 import "../style/splash.css"
-<<<<<<< HEAD
 import VideoCover from 'react-video-cover'
-=======
->>>>>>> 8d02597fa8f5bb184ec5095bd950d252504a9001
+
 
 class Splash extends React.Component {
   state = {
@@ -34,7 +32,7 @@ class Splash extends React.Component {
   }
   render(){
 
-<<<<<<< HEAD
+
     const videoOptions = {
       src: '/DisneyWalkup.webm',
       autoPlay: true,
@@ -82,37 +80,6 @@ class Splash extends React.Component {
             </div>
           </div>
         </div>
-
-=======
-    return(
-      <div className="splash">
-        <div className="splashTitle">Yelp Disney</div>
-        <div className="splashBorder">
-          <div className="splashSearch">
-            <Form className="splashSearchForm" onSubmit={this.handleSubmit}>
-              <Form.Group >
-                <label >{`Search `}
-                  <Input
-                    type="text"
-                    onChange={this.handleSearchChange}
-                    value={this.state.query}
-                    placeholder="New Search"
-                  />
-                </label>
-                <div className="splashIn">in</div>
-                <Dropdown
-                  className="splashDrop"
-                  placeholder={'All Parks'}
-                  options={Options.parkOptions}
-                  selection
-                  onChange={this.handleFilterChange}
-                />
-                <Button className="splashSubmit">Submit</Button>
-              </Form.Group>
-            </Form>
-          </div>
-        </div>
->>>>>>> 8d02597fa8f5bb184ec5095bd950d252504a9001
       </div>
     )
   }
