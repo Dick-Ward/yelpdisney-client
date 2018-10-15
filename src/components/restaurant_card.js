d57c5d7ea794abd484825a7304e07be1d9f03205
Gonzodamus <Dick@TheDickWard.com>
2018-10-15 07:30:49 -0400

    changed color on restaurant card so it doesn't look like a link

--- src/components/restaurant_card.js
+++ src/components/restaurant_card.js
@@ -14,7 +14,7 @@ const RestaurantCard = ({ restaurant, selectRestaurant, selectedRestaurant }) =>
         <div className="cardTitle">{restaurant.name} </div>
         <br/>
         <div className="cardInfo">
-          {restaurant.cuisine !== "" ? <div >- {restaurant.cuisine}</div> : <div>- Cusine Not Listed</div> }
+          {restaurant.cuisine !== "" ? <div >- {restaurant.cuisine}</div> : <div>- Cuisine Not Listed</div> }
           <div>- {Options.parseCategoryCode(restaurant.category_code)}</div>
         </div>
         <br/>
