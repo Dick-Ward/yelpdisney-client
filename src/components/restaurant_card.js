d503f5672f5afb092e440588ba87297adb3fd866
gonzodamus <github email address>
2018-10-15 10:55:48 -0400

    Kate's Issue List complete

--- src/components/restaurant_card.js
+++ src/components/restaurant_card.js
@@ -9,19 +9,14 @@ const RestaurantCard = ({ restaurant, selectRestaurant, selectedRestaurant }) =>
   return (
     <Link key={restaurant.id} to={`/restaurants/${restaurant.permalink}`}>
       <div className="restaurant-card">
-
-
         <div className="cardTitle">{restaurant.name} </div>
         <br/>
         <div className="cardInfo">
           {restaurant.cuisine !== "" ? <div >- {restaurant.cuisine}</div> : <div>- Cuisine Not Listed</div> }
           <div>- {Options.parseCategoryCode(restaurant.category_code)}</div>
-        </div>
-        <br/>
-
-
+          </div>
+          <br/>
         <div className="cardParkName">{restaurant.resort_name ? (<div> {restaurant.resort_name}</div>) : (<div > {restaurant.park}</div>)}</div>
-
       </div>
     </Link>
       );
