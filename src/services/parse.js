9188e40544a13b46a2e932c966e9b04742dd9f44
gonzodamus <github email address>
2018-10-16 17:48:53 -0400

    organized css

--- src/services/parse.js
+++ src/services/parse.js
@@ -3,7 +3,7 @@ import {startCase} from 'lodash';
 import * as moment from 'moment'
 
 export const unparsedDetails = ["opened_on", "entree_range", "breakfast_hours","lunch_hours", "dinner_hours", "parking", "wine_list"]
-export const unwantedDetails = ["average_rating", "average_value", "average_quality", "average_service", "average_cleanliness", "reviews", "name", "id", "permalink", "short_name", "code", "operator_id", "operator_type", "sort_name", "park", "operator_url", "house_specialties", "resort_name"]
+export const unwantedDetails = ["average_rating", "average_value", "average_quality", "average_service", "average_cleanliness", "reviews", "name", "id", "permalink", "park", "operator_url", "house_specialties", "resort_name"]
 
 export const getTimeZone = () =>{
   if (!sessionStorage.getItem('timezone')) {
