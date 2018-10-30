5cb56acec2d3aed69d704793f7f320fd52f292c1
Gonzodamus <Dick@TheDickWard.com>
2018-10-29 20:20:58 -0400

    fixed parse in service

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,11 +1,13 @@
 import * as moment from 'moment'
+var mom = require('moment-timezone')
+
 
 export const unparsedDetails = ["opened_on", "entree_range", "breakfast_hours","lunch_hours", "dinner_hours", "parking", "wine_list"]
 export const unwantedDetails = ["average_rating", "average_value", "average_quality", "average_service", "average_cleanliness", "reviews", "name", "id", "permalink", "park", "operator_url", "house_specialties", "resort_name"]
 
 export const getTimeZone = () =>{
   if (!sessionStorage.getItem('timezone')) {
-    let tz = moment.tz.guess() || 'UTC';
+    let tz = mom.tz.guess() || 'UTC';
     sessionStorage.setItem('timezone', tz);
   }
   return sessionStorage.getItem('timezone')
