f120cc2d56e29d3bdd13b336faa52d77d337b6c1
Gonzodamus <Dick@TheDickWard.com>
2018-11-03 16:09:15 -0400

    fixed positioning on overall number

--- src/components/restaurants/Rating.js
+++ src/components/restaurants/Rating.js
@@ -7,33 +7,35 @@ class Rating extends React.Component {
     return(
       <Grid  >
         <Grid.Row>
-          <Grid.Column width={6} className="rating-text">
+          <Grid.Column width={6}>
           </Grid.Column>
-          <Grid.Column width={4} className="rating-text">
-            <svg style={{width: 0, height: 0}}>
-              <clipPath id="svgPathOverall">
-                <ellipse cx="25" cy="30" rx="25" ry="25"/>
-                <ellipse cx="95" cy="30" rx="25" ry="25"/>
-                <ellipse cx="60" cy="80" rx="40" ry="40"/>
-              </clipPath>
-            </svg>
+          <Grid.Column width={4}>
+            <div className="ratingBox">
+              <svg style={{width: 0, height: 0}}>
+                <clipPath id="svgPathOverall">
+                  <ellipse cx="25" cy="30" rx="25" ry="25"/>
+                  <ellipse cx="95" cy="30" rx="25" ry="25"/>
+                  <ellipse cx="60" cy="80" rx="40" ry="40"/>
+                </clipPath>
+              </svg>
 
-            <svg style={{width: 0, height: 0}}>
-              <clipPath id={`ratingSVGOverall`}>
-                <text class="largeSVGText" x="10" y="95">{this.props.rating.toFixed(1)}</text>
-              </clipPath>
-            </svg>
+              <svg style={{width: 0, height: 0}}>
+                <clipPath id={`ratingSVGOverall`}>
+                  <text class="largeSVGText"y="95">{this.props.rating.toFixed(1)}</text>
+                </clipPath>
+              </svg>
 
-            <div>
-              <div className="Overall">
-                <span className="ratingFill" style={{height: `${(this.props.rating/5*100)}%`, backgroundColor:"blue"}} />
-              </div>
               <div>
-                <div className={`OverallNumber`}>
-                  <span className="numberFill" style={{height: `${100 - (this.props.rating/5*100)}%`, backgroundColor: "blue"}} />
+                <div className="Overall">
+                  <span className="ratingFill" style={{height: `${(this.props.rating/5*100)}%`, backgroundColor:"blue"}} />
                 </div>
+                <div>
+                  <div className={`OverallNumber`}>
+                    <span className="numberFill" style={{height: `${100 - (this.props.rating/5*100)}%`, backgroundColor: "blue"}} />
+                  </div>
+                </div>
+                <div> Overall </div>
               </div>
-              <div> Overall </div>
             </div>
           </Grid.Column>
           <Grid.Column width={6} className="rating-text">
